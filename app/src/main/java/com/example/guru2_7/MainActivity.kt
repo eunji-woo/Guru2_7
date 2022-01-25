825446ae4d2e2f4245ca5e07e2fbab1380af10be
Chaewon-L <yp1225@naver.com>
2022-01-25 17:21:30 +0900

    로그인 base 추가

--- app/src/main/java/com/example/guru2_7/MainActivity.kt
+++ app/src/main/java/com/example/guru2_7/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.guru2_7
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    lateinit var LoginButton: Button
+    lateinit var idEdittext: EditText
+    lateinit var pwEdittext: EditText
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        LoginButton=findViewById(R.id.LoginButton)
+        idEdittext=findViewById(R.id.idEdittext)
+        pwEdittext=findViewById(R.id.pwEdittext)
+
+
+        /*DBManager = DBManager(this, "personal", null, 1)*/
+
+        LoginButton.setOnClickListener {
+
+        }
     }
 }
