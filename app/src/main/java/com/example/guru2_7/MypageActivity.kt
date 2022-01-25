495db539e7d001e76f219a681c6aaf8fcdfd3329
Chaewon-L <yp1225@naver.com>
2022-01-25 19:06:36 +0900

    마이페이지 intent 마이페이지 변수 id 수정

--- app/src/main/java/com/example/guru2_7/MypageActivity.kt
+++ app/src/main/java/com/example/guru2_7/MypageActivity.kt
@@ -1,11 +1,28 @@
 package com.example.guru2_7
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MypageActivity : AppCompatActivity() {
+
+    lateinit var ChangePWbutton : Button
+    lateinit var Billbutton : Button
+    lateinit var Outbutton : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mypage)
+
+        ChangePWbutton = findViewById(R.id.ChangePWbutton)
+        Billbutton = findViewById(R.id.Billbutton)
+        Outbutton = findViewById(R.id.Outbutton)
+
+        ChangePWbutton.setOnClickListener{
+            // intent로 회원가입 창으로 이동
+            val intent = Intent(this, ChangeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
