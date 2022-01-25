5f853878d36c7aae7416267584f0dd912a2bb3b1
Chaewon-L <yp1225@naver.com>
2022-01-25 16:35:34 +0900

    Activity 기본

---
+++ app/src/main/java/com/example/guru2_7/Order1_Activity.kt
@@ -0,0 +1,11 @@
+package com.example.guru2_7
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Order1_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order1_)
+    }
+}
