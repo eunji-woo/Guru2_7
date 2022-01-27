82617e4af3f23f3fd472409ef3cca99a58ac7193
Chaewon-L <yp1225@naver.com>
2022-01-27 16:00:13 +0900

    네비 intent수정1

--- app/src/main/java/com/example/guru2_7/MypageFragment.kt
+++ app/src/main/java/com/example/guru2_7/MypageFragment.kt
@@ -1,5 +1,6 @@
 package com.example.guru2_7
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -21,12 +22,15 @@ class MypageFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
+
     }
 
     override fun onCreateView(
@@ -34,6 +38,8 @@ class MypageFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        val intent = Intent(getActivity(), MypageActivity::class.java)
+        startActivity(intent)
         return inflater.inflate(R.layout.fragment_mypage, container, false)
     }
 
