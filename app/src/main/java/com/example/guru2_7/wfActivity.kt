c9b4c1bd1d51968ca1facc5469db1a69b0908f1e
jhs <ghtla0330@naver.com>
2022-01-27 17:28:17 +0900

    와플대학만 스피너 추가

--- app/src/main/java/com/example/guru2_7/wfActivity.kt
+++ app/src/main/java/com/example/guru2_7/wfActivity.kt
@@ -2,15 +2,18 @@ package com.example.guru2_7
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.CheckBox
-import android.widget.LinearLayout
+import android.view.View
+import android.widget.*
 
 class wfActivity : AppCompatActivity() {
     lateinit var scrollLayout:LinearLayout
+    val spinner_wfp = findViewById<Spinner>(R.id.spinner_wfp)
+    val spinner_wft = findViewById<Spinner>(R.id.spinner_wft)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
         var dayArray = arrayOf("애플시나몬와플 3,200", "민트초코와플 4,200", "크런치딸기잼와플 4,000","티라미수와플 4,900","오레오누텔라와플 4,000",
             "바나나누텔라와플 4,500","생딸기누텔라와플 4,900","생딸기듬뿍와플 5,400","생딸기케이크와플 5,400","젤라또와플 4,700","크림젤라또와플 5,200","싸만코젤라또와플 5,500","초코범벅젤라또와플 5,200","오레오누텔라젤라또와플 5,500",
@@ -30,5 +33,33 @@ class wfActivity : AppCompatActivity() {
             checkBox.text = dayArray[i]
             scrollLayout.addView(checkBox)
         }
+
+        val place = resources.getStringArray(R.array.place_array)
+        val time = resources.getStringArray(R.array.time_array)
+
+        val Adapter_place = ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,place)
+        spinner_wfp.adapter = Adapter_place
+        spinner_wfp.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+        }
+
+        val Adapter_time = ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,time)
+        spinner_wft.adapter = Adapter_time
+        spinner_wft.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+        }
+
     }
 }
