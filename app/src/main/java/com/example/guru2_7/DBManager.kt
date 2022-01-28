3cfa16e295022b5b772d6d971fdb1f17ab9213b3
eunji-woo <eunji20392@naver.com>
2022-01-28 15:34:51 +0900

    메뉴 테이블

--- app/src/main/java/com/example/guru2_7/DBManager.kt
+++ app/src/main/java/com/example/guru2_7/DBManager.kt
@@ -18,6 +18,7 @@ import android.database.sqlite.SQLiteOpenHelper
 class DBManager(context: Context) : SQLiteOpenHelper(context, "groupDB", null, 1) {
     override fun onCreate(db: SQLiteDatabase?) {
         db!!.execSQL("CREATE TABLE groupTBL (nickname CHAR(20), email CHAR(30) PRIMARY KEY, passwd CHAR(30));")
+        db!!.execSQL("CREATE TABLE orderTBL (nickname CHAR(20) PRIMARY KEY, shop CHAR(30), menu1 CHAR(30), menu2 CHAR(30), menu3 CHAR(30), price CHAR(30), time CHAR(30), place CHAR(30));")
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, p1: Int, p2: Int) {
