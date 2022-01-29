dd6b54e33f7c91c74c063e60a849c44f62836436
eunji-woo <eunji20392@naver.com>
2022-01-29 17:55:59 +0900

    각 가게별 dbmanager

---
+++ app/src/main/java/com/example/guru2_7/kimacDBManager.kt
@@ -0,0 +1,17 @@
+package com.example.guru2_7
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class kimacDBManager(context: Context) : SQLiteOpenHelper(context, "MenuDB", null, 1) {
+    override fun onCreate(db: SQLiteDatabase?) {
+
+        db!!.execSQL("CREATE TABLE kimTBL (menu1 CHAR(30), menu2 CHAR(30), menu3 CHAR(30), menu4 CHAR(30), menu5 CHAR(30), menu6 CHAR(30), menu7 CHAR(30), menu8 CHAR(30), menu9 CHAR(30), menu10 CHAR(30));")
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, p1: Int, p2: Int) {
+        TODO("Not yet implemented")
+    }
+
+}
