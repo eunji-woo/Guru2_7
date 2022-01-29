7f34fe12a2ec9ec332a330e6b28e498525c904b7
eunji-woo <eunji20392@naver.com>
2022-01-29 18:00:37 +0900

    디비매니저 이름 수정 완료

--- app/src/main/java/com/example/guru2_7/wfDBManager.kt
+++ app/src/main/java/com/example/guru2_7/wfDBManager.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
-class wfacDBManager (context: Context) : SQLiteOpenHelper(context, "MenuDB", null, 1) {
+class wfDBManager (context: Context) : SQLiteOpenHelper(context, "MenuDB", null, 1) {
     override fun onCreate(db: SQLiteDatabase?) {
 
         db!!.execSQL("CREATE TABLE wfTBL (menu1 CHAR(30), menu2 CHAR(30), menu3 CHAR(30), menu4 CHAR(30), menu5 CHAR(30), menu6 CHAR(30), menu7 CHAR(30), menu8 CHAR(30), menu9 CHAR(30), menu10 CHAR(30));")
