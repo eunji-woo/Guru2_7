32022fd2d96a8f4c5704596457cdeeb5f9ab986b
Chaewon-L <yp1225@naver.com>
2022-01-30 15:09:11 +0900

    DBmanager 수정

--- app/src/main/java/com/example/guru2_7/DBManager.kt
+++ app/src/main/java/com/example/guru2_7/DBManager.kt
@@ -18,11 +18,6 @@ import android.database.sqlite.SQLiteOpenHelper
 class DBManager(context: Context) : SQLiteOpenHelper(context, "groupDB", null, 1) {
     override fun onCreate(db: SQLiteDatabase?) {
         db!!.execSQL("CREATE TABLE groupTBL (nickname CHAR(20), email CHAR(30) PRIMARY KEY, passwd CHAR(30));")
-        db!!.execSQL("CREATE TABLE inTBL (nickname CHAR(20) PRIMARY KEY, shop CHAR(30), menu1 CHAR(30), menu2 CHAR(30), menu3 CHAR(30), price CHAR(30), time CHAR(30), place CHAR(30));")
-        db!!.execSQL("CREATE TABLE kimTBL (nickname CHAR(20) PRIMARY KEY, shop CHAR(30), menu1 CHAR(30), menu2 CHAR(30), menu3 CHAR(30), price CHAR(30), time CHAR(30), place CHAR(30));")
-        db!!.execSQL("CREATE TABLE mongTBL (nickname CHAR(20) PRIMARY KEY, shop CHAR(30), menu1 CHAR(30), menu2 CHAR(30), menu3 CHAR(30), price CHAR(30), time CHAR(30), place CHAR(30));")
-        db!!.execSQL("CREATE TABLE wfTBL (nickname CHAR(20) PRIMARY KEY, shop CHAR(30), menu1 CHAR(30), menu2 CHAR(30), menu3 CHAR(30), price CHAR(30), time CHAR(30), place CHAR(30));")
-        db!!.execSQL("CREATE TABLE acTBL (nickname CHAR(20) PRIMARY KEY, shop CHAR(30), menu1 CHAR(30), menu2 CHAR(30), menu3 CHAR(30), price CHAR(30), time CHAR(30), place CHAR(30));")
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, p1: Int, p2: Int) {
--- app/src/main/java/com/example/guru2_7/kimDBManager.kt
+++ app/src/main/java/com/example/guru2_7/kimDBManager.kt
@@ -7,7 +7,7 @@ import android.database.sqlite.SQLiteOpenHelper
 class kimDBManager(context: Context) : SQLiteOpenHelper(context, "MenuDB", null, 1) {
     override fun onCreate(db: SQLiteDatabase?) {
 
-        db!!.execSQL("CREATE TABLE kimTBL (menu1 CHAR(30), menu2 CHAR(30), menu3 CHAR(30), menu4 CHAR(30), menu5 CHAR(30), menu6 CHAR(30), menu7 CHAR(30), menu8 CHAR(30), menu9 CHAR(30), menu10 CHAR(30));")
+        db!!.execSQL("CREATE TABLE kimTBL (menu1 CHAR(30), menu2 CHAR(30), menu3 CHAR(30), menu4 CHAR(30), menu5 CHAR(30), menu6 CHAR(30), menu7 CHAR(30), menu8 CHAR(30), menu9 CHAR(30), menu10 CHAR(30), price INT(30));")
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, p1: Int, p2: Int) {
