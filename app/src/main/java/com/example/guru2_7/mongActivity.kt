aa19300eb79e90e19d7025386ce8fb8b061de492
Chaewon-L <yp1225@naver.com>
2022-01-30 15:58:40 +0900

    price DB 수정.

--- app/src/main/java/com/example/guru2_7/mongActivity.kt
+++ app/src/main/java/com/example/guru2_7/mongActivity.kt
@@ -105,7 +105,7 @@ class mongActivity : AppCompatActivity() {
 
         okButton.setOnClickListener {
             sqlDB = dbManager.writableDatabase
-            sqlDB.execSQL("INSERT INTO acTBL VALUES ('"+menu1+"', '"+menu2+"', '"+menu3+"', '"+menu4+"', '"+menu5+"', '"+menu6+"', '"+menu7+"', '"+menu8+"', '"+menu9+"', '"+menu10 +"', '"+price+"');")
+            sqlDB.execSQL("INSERT INTO mongTBL VALUES ('"+menu1+"', '"+menu2+"', '"+menu3+"', '"+menu4+"', '"+menu5+"', '"+menu6+"', '"+menu7+"', '"+menu8+"', '"+menu9+"', '"+menu10 +"', '"+price+"');")
             val intent = Intent(this, Order1_Activity::class.java)
             intent.putExtra("shop_name", "mong")
             startActivity(intent)
