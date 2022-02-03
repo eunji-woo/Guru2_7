515382a317d76ce08fe86d5c4ad96d4e576e3bfc
Chaewon-L <yp1225@naver.com>
2022-02-03 16:10:56 +0900

    result Activity xml수정2

--- app/src/main/java/com/example/guru2_7/ResultActivity.kt
+++ app/src/main/java/com/example/guru2_7/ResultActivity.kt
@@ -1,11 +1,26 @@
 package com.example.guru2_7
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ResultActivity : AppCompatActivity() {
+    lateinit var Homebutton:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        Homebutton=findViewById(R.id.Homebutton)
+
+
+        Homebutton.setOnClickListener{
+            // Home 으로 이동
+            val intent = Intent(this, NaviActivity::class.java)
+            startActivity(intent)
+        }
     }
+
+
 }
